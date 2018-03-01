b295e811b6779d33bc3a85c64b62da506d6c3889
Cedric Tong <ctong@calstatela.edu>
2018-03-01 15:23:37 -0800

    updated logic and code; new interaction model

--- skill/food_finder.py
+++ skill/food_finder.py
@@ -3,6 +3,7 @@ import requests
 import json
 import credentials
 
+data = {}
 # --------------- Helpers that build all of the responses ----------------------
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
@@ -40,39 +41,46 @@ def get_welcome_response():
 	speech_output = "Welcome to Food Finder. What would you like to eat?"
 	reprompt_text = None
 	should_end_session = False
-	return build_response(session_attributes, build_speechlet_response(speech_output, should_end_session))
+	return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
 def handle_session_end_request():
 	card_title = "Session Ended"
 	speech_output = "Thank you for using Food Finder!"
 	should_end_session = True
-	return build_response({}, build_speechlet_response(speech_output, should_end_session))
+	return build_response({}, build_speechlet_response(card_title, speech_output, None, should_end_session))
 
 # --------------- Intents ------------------
 
 def food_recommendation_intent(intent, session):
     session_attributes = session.get('attributes', {})
+    print(data)
+    print('this is the event response:\n\n {}'.format(intent))
+    dialog_state = intent['dialogState']
+    intent = intent['intent']
     should_end_session = False
     speech_output = ''
+    reprompt_text = speech_output
     category = ''
     food = ''
-    reprompt_text = 'Is there anything else you want to find?'
     print('food finder is running')
     if 'category' in intent['slots'] or 'Food' in intent['slots']:
         foods = intent['slots']
         print(foods)
-        if 'value' in foods['category']:
-            category = foods['category']['value']
-        else:
-            category = None
-        if 'value' in foods['Food']:
+        if 'value' in foods['Category'] and 'value' in foods['Food']:
+            should_end_session = True
+            category = foods['Category']['value']
             food = foods['Food']['value']
-        places = yelp_conn(category, food)
-        if category == None:
-            speech_output = ''.join([speech_output, 'You can find delicious {} at {} on {}'.format(food, places[0]['name'], places[0]['location']['address1'])])
-        else:
+            places = yelp_conn(category, food)
             speech_output = ''.join([speech_output, 'You can find delicious {} {} at {} on {}'.format(category, food, places[0]['name'], places[0]['location']['address1'])])
-        print(places)
+            return build_response(session_attributes, build_speechlet_response('Food Finder', speech_output, reprompt_text, should_end_session))
+        else:
+            if dialog_state in ('STARTED', 'IN_PROGRESS'):
+                return continue_dialog()
+            elif dialog_state == 'COMPLETED':
+                should_end_session = True
+                print('ending here')
+                return build_response(session_attributes, build_speechlet_response('Food Finder', speech_output, reprompt_text, should_end_session))
+            speech_output = ''.join([speech_output, 'Try to specify what you want to eat!'])
     return build_response(session_attributes, build_speechlet_response('Food Finder', speech_output, reprompt_text, should_end_session))
 
 # --------------- Helper Functions ------------------
@@ -82,30 +90,42 @@ def yelp_conn(category, term):
     params = { 'location': 'Los Angeles, CA',
                'term': term,
                'categories': category,
-               'sort_by': 'distance',
-               'limit': 5
+               'sort_by': 'rating',
+               'limit': 1
              }
     # create a python file to get access key for the Yelp API
     # if you want to use the skill for your own use
     yelp = requests.get(url = url, params = params, headers = {'Authorization': 'Bearer {}'.format(credentials.api_key)})
     yelp_json = json.loads(yelp.text)
-
     return yelp_json['businesses']
+
+def get_api_device(event_info):
+    info = {}
+    info['api'] = event_info['context']['System']['apiAccessToken']
+    info['id'] = event_info['context']['System']['device']['deviceId']
+    return info
+
+def continue_dialog():
+    message = {}
+    message['shouldEndSession'] = False
+    message['directives'] = [{'type': 'Dialog.Delegate'}]
+    print(message)
+    return build_response({}, message)
 
 # --------------- Specific Events ------------------
 
 def on_intent(intent_request, session):
-	print("on_intent requestId=" + intent_request['requestId'] + ", sessionId=" + session['sessionId'])
-	intent = intent_request['intent']
-	intent_name = intent_request['intent']['name']
-	if intent_name == "FoodRecommendationIntent":
-		return food_recommendation_intent(intent, session)
-	elif intent_name == "AMAZON.HelpIntent":
-		return get_welcome_response()
-	elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-		return handle_session_end_request()
-	else:
-		raise ValueError("Invalid intent")
+    print("on_intent requestId=" + intent_request['requestId'] + ", sessionId=" + session['sessionId'])
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+    if intent_name == "FoodRecommendationIntent":
+    	return food_recommendation_intent(intent_request, session)
+    elif intent_name == "AMAZON.HelpIntent":
+    	return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+    	return handle_session_end_request()
+    else:
+    	raise ValueError("Invalid intent")
 
 # --------------- Generic Events ------------------
 
@@ -119,16 +139,17 @@ def on_launch(launch_request, session):
 def on_session_ended(session_ended_request, session):
 	print("on_session_ended requestId=" + session_ended_request['requestId'] + ", sessionId=" + session['sessionId'])
 
-
 # --------------- Main handler ------------------
 
 def lambda_handler(event, context):
-	print("event.session.application.applicationId=" + event['session']['application']['applicationId'])
-	if event['session']['new']:
-		on_session_started({'requestId': event['request']['requestId']}, event['session'])
-	if event['request']['type'] == "LaunchRequest":
-		return on_launch(event['request'], event['session'])
-	elif event['request']['type'] == "IntentRequest":
-		return on_intent(event['request'], event['session'])
-	elif event['request']['type'] == "SessionEndedRequest":
-		return on_session_ended(event['request'], event['session'])
+    data = get_api_device(event)
+    print(data)
+    print("event.session.application.applicationId=" + event['session']['application']['applicationId'])
+    if event['session']['new']:
+    	on_session_started({'requestId': event['request']['requestId']}, event['session'])
+    if event['request']['type'] == "LaunchRequest":
+    	return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+    	return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+    	return on_session_ended(event['request'], event['session'])
